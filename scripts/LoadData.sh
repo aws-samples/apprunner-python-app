04c63db4061d5bdc4eb7be92812cd0062740a303
prabaksa <prabaksa@amazon.com>
2022-01-17 13:46:21 +0530

    Modified LoadData.sh

--- scripts/LoadData.sh
+++ scripts/LoadData.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/samples/moviedata.zip
 unzip moviedata.zip
-python3 MoviesLoadData.py
+python3 scripts/MoviesLoadData.py
 rm moviedata.json 
 rm moviedata.zip
